e7bdec66c483d1d125c37ae0ff23b3ffe03460a2
Cenzino21 <vincenzolabella2018@gmail.com>
2024-01-17 17:24:23 +0100

    Commit 2

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,28 @@
 function controlTelephoneNumber() {
     let phoneNumber = document.querySelector("#enterNumberToControl") as HTMLInputElement;
     let numberPhone = phoneNumber.value.toString();
+    /*
+    In questa istruzione uso regex (/^\d+$/) a cui passo .test(numberPhone)
+    Questa istruzione mi permette di verificare se all'interno di numberPhone ci sono solo numeri
+    e mi restituisce un valore booleano */
+    if(/^\d+$/.test(numberPhone)) {
+        if(numberPhone.length === 10) { 
+            console.log("Ottimo");
+            console.log(numberPhone);
+        } else if(numberPhone.length > 10) {   
+            /*slice è un metodo. slice(0, 10) significa che deve partire ad estrarre dalla 
+            posizione = (dunque la prima), fino ad un totale di 10 elementi, ecco perchè uso
+            il 10 nel metodo, anzichè 9... Non è di posizione, ma di quantità
+            */    
+            let newNumber = numberPhone.slice(0, 10);
+            console.log(newNumber);
+        } else {
+            console.log("Errore! Numero inserito minore di 10");
+        }
+    } else {
+        console.log("Valore inserito non corretto");
+    }
+    //console.log(numberPhone2);
+      
 }
+
